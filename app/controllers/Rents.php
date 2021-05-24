cf20f46c6d4102fcafaa64af9442d58a2cfa3cdb
abdessamad <abdessamadsouilem1@gmail.com>
2021-05-24 01:05:28 +0100

    rents vc

---
+++ app/controllers/Rents.php
@@ -0,0 +1,82 @@
+<?php 
+
+class Rents extends Controller {
+    public function __construct(){
+        $this->RentModel = $this->model('Rent');
+      }
+
+
+    public function RentVehicle(){
+        if (isset($_SESSION['loginAdmin']) && $_SESSION['loginAdmin']  === true){
+            $id=$_GET['id']; 
+            $id1=$_SESSION['id'];
+    $update = $this->RentModel->getInformation($id);
+    $user = $this->RentModel->getInformationUser($id1);
+    if($update!=null){
+        $data = [
+            'updates' => $update,
+            'user' => $user
+          ];
+        $this->view("Rents/RentVehicle",$data);
+    }else{
+        flash1('register_not',"this car is not disponible");
+         redirect("Menus/showVehicle");
+    }
+            
+        }elseif (isset($_SESSION['User']) && $_SESSION['User']  === true){
+            $id=$_GET['id']; 
+            $id1=$_SESSION['id'];
+    $update = $this->RentModel->getInformation($id);
+    $user = $this->RentModel->getInformationUser($id1);
+            if($update!=null){
+                $data = [
+                    'updates' => $update,
+                    'user' => $user
+                  ];
+                $this->view("Rents/RentVehicle",$data);
+            }else{
+                flash1('register_not',"this car is not disponible");
+                 redirect("Menus/showVehicle");
+            }
+        }else{
+            redirect("users/login");
+        }
+       
+   }
+   public function CheckRent(){
+        
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $id=$_POST['id'];
+        $véhicule_résérver = $_POST['véhicule_résérver'];
+        $véhicule_résérver_model=$_POST['model'];
+        $user_résérvé_par = $_POST['user_résérvé_par'];
+        $Disponible1=$_POST['Disponible'];
+        $date_réserve = $_POST['date_réserve'];
+        $date_fin = $_POST['date_fin'];
+        $number_of_véhicule = $_POST['number_of_véhicule'];
+        $Price = $_POST['Price'];
+        $date1=strtotime($date_réserve);
+        $date2=strtotime($date_fin);
+        $diff = abs($date2 - $date1); 
+        $years = floor($diff / (365*60*60*24)); 
+        $months = floor(($diff - $years * 365*60*60*24)
+                               / (30*60*60*24)); 
+        $days = floor(($diff - $years * 365*60*60*24 - 
+             $months*30*60*60*24)/ (60*60*24));
+        $price1=$number_of_véhicule*$Price*$days;   
+        if($this->RentModel->CheckRent($number_of_véhicule,$date_fin,$date_réserve,$user_résérvé_par,$véhicule_résérver,$price1,$véhicule_résérver_model)===true){
+            $Disponible=$Disponible1-$number_of_véhicule;
+            if($this->RentModel->updateQuantity($Disponible,$id)===true){
+             flash('register_success','Résérvation add success');
+             redirect('Menus/showVehicle');
+            } 
+          } else {
+            flash1('register_not','something went wrong , try again');
+            redirect('Menus/showVehicle');
+          }  
+   }
+}
+
+
+
+}
---
+++ app/models/Rent.php
@@ -0,0 +1,60 @@
+<?php 
+
+class Rent {
+    public $pdo;
+    public $number_of_véhicule;
+    public $date_fin;
+    public $date_réserve;
+    public $user_résérvé_par;
+    public $véhicule_résérver;
+    public $price1;
+    public function __construct(){
+      $this->pdo = new Database;
+      $this->pdo=$this->pdo->getDb();
+    }
+    public function réservation_vehicle(){
+
+    }
+    public function getInformation($id){
+      $id;
+      $query = "SELECT * FROM `véhicule` where `id`='$id'";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+            return $result;
+  }
+  public function getInformationUser($id1){
+    $id1;
+    $query = "SELECT * FROM `person` where `id`='$id1'";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute();
+          $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+          return $result;
+}
+public function CheckRent($number_of_véhicule,$date_fin,$date_réserve,$user_résérvé_par,$véhicule_résérver,$price1,$véhicule_résérver_model){
+  $query = "INSERT INTO `résérvation` (`véhicule_résérver`, `user_résérvé_par`, `date_réserve`, `date_fin`, `Price`, `number_of_véhicule`,`véhicule_résérver_model`) VALUES('$véhicule_résérver', '$user_résérvé_par', '$date_réserve', '$date_fin', '$price1', '$number_of_véhicule','$véhicule_résérver_model')";
+  $stmt = $this->pdo->prepare($query);
+  if($stmt->execute()){
+      return true;
+  }else{
+      return false;
+  }
+}
+
+public function updateQuantity($Disponible,$id){
+  if($Disponible>=1){
+    $query = "UPDATE `véhicule` SET  `Disponible` = '$Disponible'  where `id` = '$id'";
+    $stmt = $this->pdo->prepare($query);
+    if($stmt->execute()){
+        return true;
+    }else{
+        return false;
+    }
+  }else{
+    return false;
+  }
+  
+}
+
+
+}
