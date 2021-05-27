c653e4f2225e5040ad61b5026886ddbb11c58441
abdessamad <abdessamadsouilem1@gmail.com>
2021-05-28 00:32:25 +0100

    go

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,104 +1,61 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-
+<?php $upload= URLROOT . "/public/upload/"; ?>
 <style>
-.container-fluid {
-  
-     padding-right: 0px;
-    padding-left: 0px;
-  
-}
-a.li {
-  color: inherit;
-  text-decoration: inherit;
-}
-h1{
-  text-align:center;
-}
-.cards{
-  display:flex;
-  justify-content:center;
-  align-items:center;
-}
-.card{
-  margin-left:12px;
-}
-    #image{
-    width: 100%;
-    height: 550px;
-    background-image:url(<?php echo URLROOT; ?>/public/img/60a1b659bc7a2.jpg);
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: 100% 550px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-#image h1{
-    color: white;
-}
+.carousel-inner {
 
-.wow{
-    width: 268px;
-    height: 191px;
+    height: 666px;
+}
+.carousel-caption {
+    top: 226px;
 }
 </style>
 
-<div class="container-fluid">
-<div id="image">
-<h1 id="change">Rent a Vehicle</h1>
-<br>
-<br>
-<h1>The satisfaction of our customers </h1>
-<br>
-<h1>is our main goal.</h1>
-<br>
-<a href="<?php echo URLROOT; ?> /Menus/showVehicle" type="button" class="btn btn-light">ONLINE BOOKING</a>
-</div>
-<br>
-<br>
-<h1>Our Catégories</h1>
-<br>
-<div class="cards">
-<a class="li" href="#">
-<div class="card" style="width: 18rem;">
-  <img class="wow" src="<?php echo URLROOT; ?>/public/img/1.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">All Véhicles.</p>
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo URLROOT; ?>/public/img/3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Rent a Vehicle</h1>
+        <br>
+        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, facilis.</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo URLROOT; ?>/public/img/4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Quality Services</h1>
+        <br>
+        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, facilis.</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo URLROOT; ?>/public/img/5.jpg " class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Best Cars Rental in Morocco</h1>
+        <br>
+        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, facilis.</h5>
+      </div>
+    </div>
   </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
-</a>
-<a class="li" href="#">
-<div class="card" style="width: 18rem;">
-  <img src="<?php echo URLROOT; ?>/public/img/depositphotos_171122936-stock-photo-new-cars.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">All Cars.</p>
-  </div>
-</div>
-</a>
-<a class="li" href="#">
-<div class="card" style="width: 18rem;">
-  <img src="<?php echo URLROOT; ?>/public/img/ONEMOTO_L1002967-min.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">All Moto.</p>
-  </div>
-</div>
-</a>
-<a class="li" href="#">
-<div class="card" style="width: 18rem;">
-  <img src="<?php echo URLROOT; ?>/public/img/1.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">All Trucks.</p>
-  </div>
-</div>
-</a>
 
-</div>
-<br>
-<br>
-<div id="all">
 
-</div>
+
+
+
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>
 var x = document.getElementById("change");
@@ -110,16 +67,8 @@ if (x.textContent == "Rent a Vehicle") {
 } else { 
     x.textContent = "Rent a Vehicle"; 
 }}
+setInterval(swapText, 3000);
 
-const targetDiv = document.getElementById("third");
-const btn = document.getElementById("toggle");
-btn.onclick = function () {
-  if (targetDiv.style.display !== "none") {
-    targetDiv.style.display = "block";
-  } else {
-    targetDiv.style.display = "none";
-  }
-};
 </script>
 
 
