088f88877c4bc4d8d851d1ab22fb58ea3837e941
abdessamad <abdessamadsouilem1@gmail.com>
2021-06-01 00:23:16 +0100

    go

--- app/views/Menus/showVehicle.php
+++ app/views/Menus/showVehicle.php
@@ -1,5 +1,10 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <?php $upload= URLROOT . "/public/upload/"; ?>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <?php ?>
 <style>
 #create_vehicle{
